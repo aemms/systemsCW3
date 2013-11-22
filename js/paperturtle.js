5bf42becd2476e023b56dbaf8c634a8b11f6bffb
Alex Emms <me@alexemms.com>
2013-11-22 16:06:38 +0000

    Implemented with paper.js

---
+++ js/paperturtle.js
@@ -0,0 +1,172 @@
+//Start Paper Js Drawing
+var position = new Point(400,200);
+
+
+// define variables that can be changed
+var maxSpeed = 10;
+var maxAngle = 3;
+var maxThrust = 0.2;
+var maxCoast = 0.99;
+var thrustColor = "orange";
+var crazy = false;
+
+var path = new Path();
+path.strokeColor = 'black';
+path.add(position);
+
+function onFrame() {
+	if(Key.isDown('left')) {
+		Triangle.turnLeft();
+	}
+	if (Key.isDown('right')) {
+    Triangle.turnRight();
+  }
+  if (Key.isDown('up')) {
+    Triangle.thrust();
+  } else {
+    Triangle.coast();
+  }  
+	Triangle.move();	
+}
+
+project.currentStyle.strokeColor = 'black';
+
+var Triangle = new function() {
+	var path = new Path([-14, -8], [14, 0], [-14, 8], [-12, 4], [-12, -4], [-14,-8]);
+	path.fillColor = "black";
+	var thrust = new Path([-13, -4], [-30, 0], [-13, 4]);
+	thrust.strokeColor = thrustColor;
+	thrust.fillColor = thrustColor;
+	var group = new Group(path, thrust);
+  group.position = view.bounds.center;
+
+  return {
+  	item: group,
+
+  	angle: 0,
+
+  	vector: new Point({
+      angle: 0,
+      length: 0
+    }),
+
+    turnLeft: function() {
+      group.rotate(-maxAngle);
+      this.angle -= maxAngle;
+    },
+
+    turnRight: function() {
+      group.rotate(maxAngle);
+      this.angle += maxAngle;
+    },
+
+    turn180: function() {
+    	group.rotate(180);
+    	this.angle += 180;
+    },
+
+    thrust: function() {
+      thrust.visible = true;
+      this.vector += new Point({
+        angle: this.angle,
+        length: maxThrust
+      });
+      if (this.vector.length > maxSpeed) {
+        this.vector.length = maxSpeed;
+      }
+    },
+
+    stop: function() {
+      this.vector.length = 0;
+    },
+    
+    coast: function() {
+      thrust.visible = false;
+      this.vector *= maxCoast;
+    },
+    
+    move: function() {
+    	this.thrustUpdate();    
+      group.position += this.vector;
+      keepInView(group);
+    },
+
+    thrustUpdate: function() {
+    	thrust.strokeColor = thrustColor;
+			thrust.fillColor = thrustColor;
+    },
+    
+    moveTo: function(position) {
+      group.position = position;
+      keepInView(group);
+    },
+
+    resetTriangle: function() {
+    	group.position = view.bounds.center;
+    	this.vector.angle = 0;
+    	this.vector.length = 0;
+    	this.length = 0;
+    },
+
+    increase: function(){
+    	crazy = true;
+			console.log("increase clicked");	
+			maxSpeed = 200;
+			maxAngle = 6;
+			maxThrust = 2;
+			maxCoast = 0.95;
+			thrustColor = "red";
+		},
+
+		decrease: function() {
+			crazy = false;
+			maxSpeed = 10;
+			maxAngle = 3;
+			maxThrust = 0.2;
+			maxCoast = 0.99;
+			thrustColor = "orange";
+		}
+
+  }
+}
+
+function keepInView(item) {
+  var position = item.position;
+  var itemBounds = item.bounds;
+  var bounds = view.bounds;
+
+  if (itemBounds.left > bounds.width) {
+    position.x = -item.bounds.width;
+  }
+
+  if (position.x < -itemBounds.width) {
+    position.x = bounds.width;
+  }
+
+  if (itemBounds.top > view.size.height) {
+    position.y = -itemBounds.height;
+  }
+
+  if (position.y < -itemBounds.height) {
+    position.y = bounds.height  + itemBounds.height / 2;
+  }
+}
+
+function onKeyUp(event){
+	if(event.key == "space"){
+		console.log("pressed");
+		Triangle.turn180();
+	}
+	if(event.key =="r"){
+		console.log("reset");
+		Triangle.resetTriangle();
+	}
+	if(event.key == "shift"){
+		console.log("shift pressed");  		
+		if(crazy) {
+			Triangle.decrease();
+		} else {
+			Triangle.increase();
+		} 
+	}
+}
--- turtle.php
+++ turtle.php
@@ -1,27 +1,42 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Turtle Fun</title>
+    <title>Triangle Fun</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">    
   </head>
   <body>
     <div class="container">
       <h4><a href="index.php"><span class="glyphicon glyphicon-arrow-left"></span> Back</a></h4>
       <header>
-        <h1>Turtle Fun!</h1>
+        <h1>Triangle Fun!</h1>
       </header>
+      
+      <div class="row">
+        <div class="col-sm-8 col-md-8 col-lg-8" >
+          <canvas id="myCanvas" width="800" height="400" ></canvas>
+        </div>
+        <div class="col-sm-4 col-md-4 col-lg-4" >
+          <button id="myButton" type="button" class="btn btn-danger pull-right">The Red Button (Crazy Mode)</button>
+        </div>
+      </div> <!-- row -->
+      <div class="row">
+        <div class="col-sm-12 pull-left">
+          <span class="label label-default">Shift</span><p><em>Shift for danger mode</em></p>  
+        </div>        
+      </div>
 
-      <canvas id="myCanvas" width="800" height="400" ></canvas>
 
     </div> <!-- container -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/turtle.js"></script>
+    <script src="js/bootstrap.min.js"></script>    
+    <!-- <script src="js/turtle.js"></script> -->
+    <script type="text/javascript" src="bower_components/paper/dist/paper.js"></script>
+    <script type="text/paperscript" src="js/paperturtle.js" canvas="myCanvas"></script> 
   </body>
 </html> 
